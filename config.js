d9bfde11169d4b5f5b9867b687f66b355a8ec4d0
TJ Yquin <tahnee.yquin@gmail.com>
2020-10-20 17:53:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -100,6 +100,18 @@ var config = {
 				broadcastNewsUpdates: true
 			}
 		},
+		{
+			module: 'MMM-Remote-Control',
+			// uncomment the following line to show the URL of the remote control on the mirror
+			position: 'bottom_left',
+			// you can hide this module afterwards from the remote control itself
+			config: {
+					customCommand: {},  // Optional, See "Using Custom Commands" below
+					customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
+					showModuleApiMenu: true, // Optional, Enable the Module Controls menu
+					apiKey: "",         // Optional, See API/README.md for details
+			}
+		},
 	]
 };
 
